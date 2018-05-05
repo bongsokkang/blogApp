c67cf3ed16ef6f54b3c57e12bc005bd6e9205f32
Hackerman <daniel@mydeveloperjourney.com>
2018-05-04 21:40:59 -0700

    fixes bug and how has CR functionality

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PostsRouter = express.Router();
 const Post = require('../models/post');
 const mongoose = require('../db/mongoose');
-const _ = require('lodash');;
+const _ = require('lodash');
 
 PostsRouter.get('/', (req, res) => {
   Post.find().then((posts) => {
@@ -12,17 +12,20 @@ PostsRouter.get('/', (req, res) => {
     });
   }).catch((error) => {
     res.status(400).render('errors/400')
+    // res.send(error); When testing in Postman
   });
 });
 
 PostsRouter.post('/', (req, res) => {
-  let body = _.pick(req.body, ['content'])
-  let newPost = new Post(body);
+  let newPost = new Post({
+    content: req.body.content
+  });
   newPost.save().then((post) => {
-    // res.json(post) For testing with Postmamn
+    // res.json(post) // For testing with Postman
     res.redirect('/posts')
   }).catch((error) => {
     res.status(400).render('errors/400');
+    // res.send(error); // For testing with Postman
   });
 });
 
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const morgan = require('morgan');
 const PostsRouter = require('./routes/posts');
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/posts', PostsRouter);
-
 app.use(morgan('tiny'));
-app.set('view engine', 'ejs')
-
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
 
 
 
