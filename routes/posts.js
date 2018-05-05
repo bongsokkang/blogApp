ee7a041b73fb6df2c2c058fffe289f653834b89d
Hackerman <daniel@mydeveloperjourney.com>
2018-05-04 21:19:12 -0700

    adds addtional logic

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const PostsRouter = express.Router();
-const Post = require('../models/post')
-const mongoose = require('../db/mongoose')
+const Post = require('../models/post');
+const mongoose = require('../db/mongoose');
+const _ = require('lodash');;
 
 PostsRouter.get('/', (req, res) => {
   Post.find().then((posts) => {
-    res.send(posts)
+    // res.json(posts) When testing route in Postman
+    res.render('posts', {
+      posts: posts
+    });
   }).catch((error) => {
     res.status(400).render('errors/400')
   });
 });
 
+PostsRouter.post('/', (req, res) => {
+  let body = _.pick(req.body, ['content'])
+  let newPost = new Post(body);
+  newPost.save().then((post) => {
+    // res.json(post) For testing with Postmamn
+    res.redirect('/posts')
+  }).catch((error) => {
+    res.status(400).render('errors/400');
+  });
+});
 
 
 
